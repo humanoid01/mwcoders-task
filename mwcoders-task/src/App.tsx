9cc88988b6004ed7d085fe95495dfa2527b0119d
Stolarek <przemyslaw.stolarek1@gmail.com>
2023-01-04 11:35:01 +0100

    Create .env, add routing, style and expand Navbar.tsx

--- mwcoders-task/src/App.tsx
+++ mwcoders-task/src/App.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { Calculator } from './components/Calculator/Calculator';
+import { MainPage } from './components/MainPage/MainPage';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path='mwcoders-task' element={<MainPage />} />
+        <Route path='mwcoders-task/calculator' element={<Calculator />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- mwcoders-task/src/components/Navbar/Navbar.tsx
+++ mwcoders-task/src/components/Navbar/Navbar.tsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.scss';
+
+const linkStyle = {
+  textDecoration: 'none',
+  color: '#ffffff',
+};
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className='nav'>
+      <Link style={linkStyle} to='mwcoders-task'>
+        <button className='btn'>Main Page</button>
+      </Link>
+      <Link style={linkStyle} to='mwcoders-task/calculator'>
+        <button className='btn'>Calculator</button>
+      </Link>
+    </div>
+  );
 };
